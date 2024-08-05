8c833961eca1cba0883c2636258ce0d3e1c595ea
Niilo Keinänen <niilo.keinanen@lightningchart.com>
2024-08-05 10:06:37 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,74 @@
+# Gauge chart dashboard
+
+![Gauge chart dashboard](gaugeDashboard-darkGold.png)
+
+This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
+
+LightningChart JS is entirely GPU accelerated and performance optimized charting library for presenting massive amounts of data. It offers an easy way of creating sophisticated and interactive charts and adding them to your website or web application.
+
+The demo can be used as an example or a seed project. Local execution requires the following steps:
+
+-   Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+-   Open the project folder in a terminal:
+
+          npm install              # fetches dependencies
+          npm start                # builds an application and starts the development server
+
+-   The application is available at _http://localhost:8080_ in your browser, webpack-dev-server provides hot reload functionality.
+
+
+## Description
+
+This example shows gauge charts on a dashboard with real-time data.
+
+In this example, a custom angle interval is defined for the gauges:
+
+```javascript
+gauge.setAngleInterval(180, 0)
+// Other common intervals:
+// .setAngleInterval(135, 45)
+// .setAngleInterval(225, -45)
+// .setAngleInterval(270, -90)
+```
+
+The rounded edges of the gauge bar and value indicators can be disabled by:
+
+```javascript
+gauge.setRoundedEdges(false)
+```
+
+
+## API Links
+
+* [Gauge Chart]
+* [XY Chart]
+* [Line Series]
+* [Axis]
+
+
+## Support
+
+If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
+
+Official [API documentation][1] can be found on [LightningChart][2] website.
+
+If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
+
+If you think you found a bug in the LightningChart JavaScript library, please contact sales@lightningchart.com.
+
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@lightningchart.com.
+
+[0]: https://github.com/Arction/
+[1]: https://lightningchart.com/lightningchart-js-api-documentation/
+[2]: https://lightningchart.com
+[3]: https://stackoverflow.com/questions/tagged/lightningchart
+[4]: https://lightningchart.com/support-services/
+
+© LightningChart Ltd 2009-2022. All rights reserved.
+
+
+[Gauge Chart]: https://lightningchart.com/js-charts/api-documentation/v6.0.0/classes/GaugeChart.html
+[XY Chart]: https://lightningchart.com/js-charts/api-documentation/v6.0.0/classes/ChartXY.html
+[Line Series]: https://lightningchart.com/js-charts/api-documentation/v6.0.0/classes/LineSeries.html
+[Axis]: https://lightningchart.com/js-charts/api-documentation/v6.0.0/classes/Axis.html
+
---
+++ src/index.js
@@ -0,0 +1,110 @@
+const lcjs = require('@lightningchart/lcjs')
+const { lightningChart, Themes, AxisScrollStrategies, AxisTickStrategies, emptyFill, emptyLine } = lcjs
+
+// Create HTML elements for the dashboard layout
+const exampleContainer = document.getElementById('chart') || document.body
+if (exampleContainer === document.body) {
+    exampleContainer.style.width = '100vw'
+    exampleContainer.style.height = '100vh'
+    exampleContainer.style.margin = '0px'
+}
+exampleContainer.style.display = 'flex'
+exampleContainer.style.flexDirection = 'row'
+const gaugeLayout = document.createElement('div')
+exampleContainer.append(gaugeLayout)
+gaugeLayout.style.width = '25%'
+gaugeLayout.style.height = '100%'
+const xyContainer = document.createElement('div')
+exampleContainer.append(xyContainer)
+xyContainer.style.width = '75%'
+xyContainer.style.height = '100%'
+
+// Initialize LightningChart JS
+const lc = lightningChart({
+            resourcesBaseUrl: new URL(document.head.baseURI).origin + new URL(document.head.baseURI).pathname + 'resources/',
+        })
+
+const gaugeChartArray = []
+const lineSeriesArray = []
+
+// Create XY chart for line series
+const xyChart = lc
+    .ChartXY({
+        container: xyContainer,
+        theme: Themes[new URLSearchParams(window.location.search).get('theme') || 'darkGold'] || undefined,
+    })
+    .setTitle('')
+    .setCursor((cursor) => cursor.setTickMarkerXVisible(false))
+xyChart
+    .getDefaultAxisX()
+    .setTickStrategy(AxisTickStrategies.Empty)
+    .setThickness(0)
+    .setStrokeStyle(emptyLine)
+    .setScrollStrategy(AxisScrollStrategies.progressive)
+    .setInterval({ start: 0, end: 10_000, stopAxisAfter: false })
+xyChart.getDefaultAxisY().dispose()
+
+// Create dashboard rows
+for (let iCh = 0; iCh < 4; iCh++) {
+    const axisY = xyChart
+        .addAxisY({ iStack: 4 - iCh })
+        .setMargins(10, 10)
+        .setInterval({ start: 0, end: 100 })
+    const lineSeries = xyChart
+        .addPointLineAreaSeries({ axisY, dataPattern: 'ProgressiveX' })
+        .setMaxSampleCount(10_000)
+        .setAreaFillStyle(emptyFill)
+    lineSeriesArray.push(lineSeries)
+
+    const gaugeContainer = document.createElement('div')
+    gaugeLayout.append(gaugeContainer)
+    gaugeContainer.style.height = '25%'
+    const gauge = lc
+        .Gauge({
+            container: gaugeContainer,
+            theme: Themes[new URLSearchParams(window.location.search).get('theme') || 'darkGold'] || undefined,,
+        })
+        .setTitle('')
+        .setUnitLabel(`Channel ${iCh + 1}`)
+        .setInterval(0, 100)
+        .setAngleInterval(180, 0)
+        .setRoundedEdges(false)
+        .setBarThickness(20)
+        .setNeedleLength(20)
+        .setNeedleThickness(5)
+        .setValueIndicatorThickness(10)
+        .setGapBetweenBarAndValueIndicators(1)
+        .setTickFormatter((tick) => tick.toFixed(0))
+        .setValueLabelFont((font) => font.setSize(24))
+        .setUnitLabelFont((font) => font.setSize(16))
+        .setTickFont((font) => font.setSize(16))
+    gaugeChartArray.push(gauge)
+}
+
+// Construct value indicator array from a color palette
+const valueIndicators = []
+const colorPalette = gaugeChartArray[0].getTheme().examples.badGoodColorPalette
+const intervalStart = 0
+const intervalEnd = 100
+const stepSize = (intervalEnd - intervalStart) / colorPalette.length
+colorPalette.forEach((color, index) => {
+    valueIndicators.push({
+        start: intervalStart + stepSize * index,
+        end: intervalStart + stepSize * (index + 1),
+        color,
+    })
+})
+gaugeChartArray.forEach((gauge) => gauge.setValueIndicators(valueIndicators))
+
+// Generate random real-time data
+const prevValues = lineSeriesArray.map((_) => Math.random() * 100)
+setInterval(() => {
+    const currentTime = performance.now()
+    for (let iCh = 0; iCh < 4; iCh++) {
+        const prev = prevValues[iCh]
+        const currentValue = Math.max(Math.min(prev + 5 * (Math.random() * 2 - 1), 100), 0)
+        prevValues[iCh] = currentValue
+        lineSeriesArray[iCh].add({ x: currentTime, y: currentValue })
+        gaugeChartArray[iCh].setValue(currentValue)
+    }
+}, 1000 / 60)
